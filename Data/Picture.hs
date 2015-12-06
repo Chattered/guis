f64369c8d60da69391a41ac27428ae2e779a356c
Phil Scott <me@philscotted.com>
2015-12-06 17:32:18 +0000

    Use Generic for Binary instance.

--- Data/Picture.hs
+++ Data/Picture.hs
@@ -1,31 +1,27 @@
-{-# LANGUAGE DeriveFunctor, GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Data.Picture (Angle(..), Picture(..)) where
 
 import Control.Monad
 import Data.Binary
+import GHC.Generics
 import Philed.Data.NNeg (NNeg)
 import Philed.Data.Rect
 import Philed.Data.Vector
 import Test.QuickCheck.Arbitrary
 import Test.QuickCheck.Gen
 
-newtype Angle = Angle Double deriving (Eq,Show)
+newtype Angle = Angle Double deriving (Arbitrary,Binary,CoArbitrary,Eq,Show)
 
 data Picture i = Image i
                | Translate (Vec Double) (Picture i)
                | Scale (NNeg Double,NNeg Double) (Picture i)
                | Rotate Angle (Picture i)
                | ReflectHorizontal (Picture i)
-               deriving (Eq,Functor,Show)
+               deriving (Eq,Functor,Generic,Show)
 
-instance Binary i => Binary (Picture i) where
-  put (Image i)             = put (0::Word8) >> put i
-  put (Translate (x,y) pic) = put (1::Word8) >> put x >> put y >> put pic
-  get = do
-    discriminator <- get :: Get Word8
-    case discriminator of
-     0 -> Image <$> get
-     1 -> Translate <$> ((,) <$> get <*> get) <*> get
+instance Binary i => Binary (Picture i)
 
 instance Arbitrary i => Arbitrary (Picture i) where
   arbitrary = frequency [(1, liftM Image arbitrary)
