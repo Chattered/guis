b53964d229fc2d95190ad37cc8521e7637356fac
Phil Scott <me@philscotted.com>
2016-02-12 17:38:57 +0000

    Update for latest unstable.

--- default.nix
+++ default.nix
@@ -1,8 +1,11 @@
-{ haskellngPackages ? (import <nixpkgs> {}).haskellngPackages,
-  pkgs ? (import <nixpkgs> {}).pkgs
+{ pkgs ? (import <nixpkgs> {}).pkgs
 }:
 let
-  env = haskellngPackages.ghcWithPackages (p: with p; [
+  myemacs =
+    let emacsWithImageMagick = with pkgs; recurseIntoAttrs (emacsPackagesNgGen (pkgs.emacs.override { imagemagick = pkgs.imagemagick; })); in
+    with pkgs.emacsPackages; emacsWithImageMagick.emacsWithPackages
+      [ pkgs.haskellPackages.ghc-mod haskellMode magit emacsw3m ];
+  myhaskell = pkgs.haskellPackages.ghcWithPackages (p: with p; [
     binary byteorder bytestring containers exceptions free lens mmorph mtl
     PhiledCommon pipes pipes-binary pipes-bytestring pipes-safe QuickCheck sdl2
     sdl2-ttf transformers unix
@@ -10,6 +13,6 @@ let
   ]);
 in pkgs.stdenv.mkDerivation {
   name = "GUIS";
-  buildInputs = [ env pkgs.dejavu_fonts ];
+  buildInputs = [ myemacs myhaskell pkgs.dejavu_fonts ];
   fonts = pkgs.dejavu_fonts;
 }
